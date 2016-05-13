7bee8413ee32c3c9294b82eb78484692e9feb9a7
Bill DeCoste <bdecoste@gmail.com>
2016-05-13 10:17:14 -0700

    update

--- dynamicvdb-datafederation/setup.sh
+++ dynamicvdb-datafederation/setup.sh
@@ -1,6 +1,6 @@
 echo "!!!!!!!!!!!!!!!!!!!! running"
 cp -rf ${HOME}/source/dynamicvdb-datafederation/src/teiidfiles ${JBOSS_HOME}
-${JBOSS_HOME}/bin/jboss-cli.sh --connect --file=${HOME}/source/dynamicvdb-datafederation/src/scripts/setup.cli 
+${JBOSS_HOME}/bin/jboss-cli.sh --connect controller=${HOSTNAME} --file=${HOME}/source/dynamicvdb-datafederation/src/scripts/setup.cli 
 cp -rf ${HOME}/source/dynamicvdb-datafederation/src/vdb/* ${JBOSS_HOME}/standalone/deployments
 echo "!!!!!!!!!!!!!!!!!!!!!!!! ran"
 
