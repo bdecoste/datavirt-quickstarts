d042e0494d406a6f2efa9cd64c684a0cd9428c78
Bill Decoste <wdecoste@redhat.com>
2016-07-25 07:47:36 -0700

    test

--- dynamicvdb-datafederation/setup.sh
+++
@@ -1,5 +0,0 @@
-echo "!!!!!!!!!!!!!!!!!!!! running"
-cp -rf ${HOME}/source/dynamicvdb-datafederation/src/teiidfiles ${JBOSS_HOME}
-cp -rf ${HOME}/source/dynamicvdb-datafederation/src/vdb/* ${JBOSS_HOME}/standalone/deployments
-echo "!!!!!!!!!!!!!!!!!!!!!!!! ran"
-
